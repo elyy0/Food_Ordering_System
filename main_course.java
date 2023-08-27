10ceed7988f7e1c43b987db5ee3be0fb5e2429bc
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-27 23:15:28 +0530

    main_course.java

---
+++ main_course.java
@@ -0,0 +1,23 @@
+package order;
+import java.util.*;
+public class main_course extends introduction {
+	public static void main(String[] args) {
+	}
+	public void main_course()
+	{
+		A="Angara Kabab";
+		B="Chicken Double Ekka";
+		C="Chicken Behtareen";
+		D="Chicken Lajawab Kabab";
+		pr1=550;
+		pr2=550;
+		pr3=550;
+		pr4=500;
+		System.out.println("MAIN COURSE");
+		System.out.println("Item A:"+A+"\t\tRs."+pr1);
+		System.out.println("Item B:"+B+"\tRs."+pr2);
+		System.out.println("Item C:"+C+"\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}
+}
