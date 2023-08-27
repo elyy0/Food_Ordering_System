d6ed432f8efeaa7a4b19624eb711f80aca1e402b
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-27 23:06:21 +0530

    starters.java

---
+++ starters.java
@@ -0,0 +1,23 @@
+package order;
+import java.util.*;
+public class starters extends introduction{
+	public static void main(String[] args) {
+	}
+	public static void starters()
+	{
+		A="Chicken Afghani";
+		B="Chicken 365";
+		C="Chicken Shanghai";
+		D="Chicken Crispy";
+		pr1=350;
+		pr2=350;
+		pr3=350;
+		pr4=350;
+		System.out.println("STARTERS");
+		System.out.println("Item A:"+A+"\tRs."+pr1);
+		System.out.println("Item B:"+B+"\tRs."+pr2);
+		System.out.println("Item C:"+C+"\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}	
+}
