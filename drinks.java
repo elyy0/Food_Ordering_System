99c2236ef6888dd3c82046a9b18a30baa82a477a
ILMA MALIK <26ilmamalik@gmail.com>
2023-08-27 23:18:25 +0530

    drinks.java

---
+++ drinks.java
@@ -0,0 +1,23 @@
+package order;
+import java.util.*;
+public class drinks extends introduction {
+	public static void main(String[] args) {
+	}
+	public void drinks() 
+	{
+		A="Coke";
+		B="Thumbs-up";
+		C="Sprite";
+		D="Mountain Dew";
+		pr1=40;
+		pr2=40;
+		pr3=40;
+		pr4=40;
+		System.out.println("DRINKS");
+		System.out.println("Item A:"+A+"\t\tRs."+pr1);
+		System.out.println("Item B:"+B+"\tRs."+pr2);
+		System.out.println("Item C:"+C+"\t\tRs."+pr3);
+		System.out.println("Item D:"+D+"\tRs."+pr4);
+		select_menu_item();
+	}
+}
